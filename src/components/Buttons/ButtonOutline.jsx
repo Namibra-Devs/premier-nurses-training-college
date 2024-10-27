fbe563f45f54d312e075b36df83000b73d3df6f0
Tyler Gobka Bright <tgbright88@gmail.com>
2024-10-27 15:49:35 +0000

    Add Button outline

---
+++ src/components/Buttons/ButtonOutline.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ButtonOutline = () => {
+  return (
+    <div>
+      <button className="bg-transparent hover:bg-white border-2 border-white hover:border-white border-transparent duration-700 ease text-white hover:text-primary px-6 py-2 rounded-full text-sm uppercase font-medium">
+        Request Info
+      </button>
+    </div>
+  );
+};
+
+export default ButtonOutline;
