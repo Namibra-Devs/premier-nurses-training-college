5ec9296c961656aeb550c54b32bc155163e4fc26
Tyler Gobka Bright <tgbright88@gmail.com>
2024-11-06 22:07:25 +0000

    Removed Odd Affiliates Logos

--- src/components/UtilityComponents/AffiliateLogos.jsx
+++ src/components/UtilityComponents/AffiliateLogos.jsx
@@ -2,11 +2,6 @@
 import Logo1 from "../../assets/accreditedlogos/logo1.png";
 import Logo2 from "../../assets/accreditedlogos/logo2.png";
 import Logo3 from "../../assets/accreditedlogos/logo3.png";
-import Logo4 from "../../assets/accreditedlogos/logo4.png";
-import Logo5 from "../../assets/accreditedlogos/logo5.png";
-import Logo6 from "../../assets/accreditedlogos/logo6.png";
-import Logo7 from "../../assets/accreditedlogos/logo7.png";
-import Logo8 from "../../assets/accreditedlogos/logo8.png";
 
 // Export an array of logos directly
-export const Logos = [Logo1, Logo2, Logo3, Logo4, Logo5, Logo6, Logo7, Logo8];
+export const Logos = [Logo1, Logo2, Logo3];
