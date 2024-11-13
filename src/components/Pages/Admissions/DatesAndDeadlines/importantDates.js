ecc74b0c50c38a7e1599948e711bc3ac2dda6807
Tyler Gobka Bright <tgbright88@gmail.com>
2024-11-13 16:35:26 +0000

    Added Dates and Deadlines, Aid and Scholarship Section

---
+++ src/components/Pages/Admissions/DatesAndDeadlines/importantDates.js
@@ -0,0 +1,26 @@
+// importantDates.js
+const importantDates = [
+    {
+      title: "Application Deadline",
+      date: "March 31, 2024",
+      description: "Last date to submit your application."
+    },
+    {
+      title: "Document Submission Deadline",
+      date: "April 10, 2024",
+      description: "Deadline to submit all required documents."
+    },
+    {
+      title: "Enrollment Confirmation Deadline",
+      date: "May 1, 2024",
+      description: "Deadline for enrolling after receiving admission."
+    },
+    {
+        title: "Fee Payment Deadline",
+        date: "July 30, 2024",
+        description: "Final date to clear any outstanding balance before enrollment."
+      }
+  ];
+  
+  export default importantDates;
+  
