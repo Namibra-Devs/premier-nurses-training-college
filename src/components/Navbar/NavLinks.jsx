10cc42a848d1dbf7bc65d8f5865224c2654262f6
Tyler Gobka Bright <tgbright88@gmail.com>
2024-11-19 13:03:39 +0000

    developing...

--- src/components/Navbar/NavLinks.jsx
+++ src/components/Navbar/NavLinks.jsx
@@ -4,17 +4,20 @@ import { links } from "./Mylinks";
 const NavLinks = () => {
   return (
     <>
-      {links.map((link, linkIndex) => (
+      {links.map((menu, linkIndex) => (
         <div key={linkIndex}>
-          {/* Desktop menus */}
-          <div className="text-left md:cursor-pointer group">
+          {/* Desktop menus or links */}
+          <div className="text-left md:cursor-pointer">
             <Link
-              to={link.link}
-              className="block hover:text-yellow-400 duration-500"
+              to={menu.link}
+              className="block hover:text-yellow-400 duration-500 group"
             >
-              <h1 className="font-normal flex justify-between items-center mr-4 py-3 md:py-0 group">
-                {link.name}
+              <h1 className="font-normal flex justify-between items-center mr-4 py-3 md:py-0 group relative">
+                {menu.name}
               </h1>
+              <span className="hidden md:flex absolute group-hover:text-yellow-400 opacity-0 group-hover:opacity-100 duration-700 ease-in-out">
+                <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
+              </span>
             </Link>
           </div>
         </div>
