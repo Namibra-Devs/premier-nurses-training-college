266edf286a736c5b2cae79c532b2b3c7c8d6be2d
Tyler Gobka Bright <tgbright88@gmail.com>
2024-11-21 11:14:23 +0000

    Login and Registeration Navigation

---
+++ src/components/Pages/Admissions/Application/Login/ButtonLogin.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+const ButtonLogin = ({url, label}) => {
+  return (
+    <Link to={url}>
+      <button
+        className="bg-transparent border-2 border-white hover:bg-primary hover:border-primary rounded-3xl hover:shadow-custom-light 
+      duration-500 ease text-white px-8 py-2 text-sm uppercase font-medium hover:scale-110 md:ml-3"
+      >
+        {label}
+      </button>
+    </Link>
+  )
+}
+
+export default ButtonLogin
