4b8bdadb441edbb8118eff359a944163dd0d2bf6
Tyler Gobka Bright <tgbright88@gmail.com>
2024-11-21 09:38:44 +0000

    Resolve Logo Issue

--- src/components/Pages/Admissions/Application/Login/LoginPage.jsx
+++ src/components/Pages/Admissions/Application/Login/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../../../../../assets/logo.png";
+import logo from "../../../../../assets/Logo.png";
 import SubmitButton from "../../../../Buttons/SubmitButton";
 import OverlayAlert from "../OverlayAlert";
 
