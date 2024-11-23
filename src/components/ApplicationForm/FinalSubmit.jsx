a981b32f35df877d9bc16cfd5b2827df86397e11
Tyler Gobka Bright <tgbright88@gmail.com>
2024-11-23 22:17:06 +0000

    Added Applicform

---
+++ src/components/ApplicationForm/FinalSubmit.jsx
@@ -0,0 +1,12 @@
+const FinalSubmit = ({ onSubmit }) => (
+    <div>
+      <h2 className="text-xl font-bold mb-4">Submit Application</h2>
+      <p>Please review your application before submission.</p>
+      <button onClick={onSubmit} className="bg-green-500 text-white py-2 px-4 mt-4 rounded">
+        Submit Application
+      </button>
+    </div>
+  );
+  
+  export default FinalSubmit;
+  
---
+++ src/components/ApplicationForm/ProgramChoice.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProgramChoice = () => {
+  return (
+    <div>ProgramChoice</div>
+  )
+}
+
+export default ProgramChoice
