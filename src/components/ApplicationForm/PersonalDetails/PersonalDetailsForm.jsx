51df91186ba17282ec715107a9d2aa409405e3e9
Tyler Gobka Bright <tgbright88@gmail.com>
2024-11-26 00:00:14 +0000

    developing...

--- src/components/ApplicationForm/PersonalDetails/PersonalDetailsForm.jsx
+++ src/components/ApplicationForm/PersonalDetails/PersonalDetailsForm.jsx
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from "react";
 import ProfilePictureUpload from "./ProfilePictureUpload";
 import SaveButton from "../Buttons/SaveButton";
+import OverlayAlert from "../FormControls/OverlayAlert";
 
-const PersonalDetailsForm = ({ preview, setPreview, onSave }) => {
+const savePersonalDetails = (personalDetail) => {
+  localStorage.setItem("personalDetail", JSON.stringify(personalDetail));
+};
+
+const retrivePersonalDetails = () => {
+  const personalDetail = localStorage.getItem("personalDetail");
+  if (personalDetail) {
+    return JSON.parse(personalDetail);
+  }
+  return {};
+};
+
+const PersonalDetailsForm = ({ preview, setPreview }) => {
   const [isVisible, setIsVisible] = useState(false);
 
   // Load program data on component mount
@@ -10,269 +23,278 @@ const PersonalDetailsForm = ({ preview, setPreview, onSave }) => {
     setTimeout(() => setIsVisible(true), 100); // Trigger the animation
   }, []);
 
-  const [formData, setFormData] = useState({
-    profilePicture: null,
-    // Personal Information--------
-    surname: "",
-    firstName: "",
-    otherName: "",
-    gender: "",
-    dateOfBirth: "",
-    age: "",
-    placeOfBirth: "",
-    religion: "",
-    hometown: "",
-    district: "",
-    region: "",
-    maritalStatus: "Single",
-    numberOfChildren: "",
-    healthCondition: "",
-    Disability: "",
-    languagesSpoken: "",
-    // End Personal Information
+  // const [personalDetail, setpersonalDetail] = useState({
+  //   profilePicture: null,
+  //   // Personal Information--------
+  //   surname: "",
+  //   firstName: "",
+  //   otherName: "",
+  //   gender: "",
+  //   dateOfBirth: "",
+  //   age: "",
+  //   placeOfBirth: "",
+  //   religion: "",
+  //   hometown: "",
+  //   district: "",
+  //   region: "",
+  //   maritalStatus: "Single",
+  //   numberOfChildren: "",
+  //   healthCondition: "",
+  //   Disability: "",
+  //   languagesSpoken: "",
+  //   // End Personal Information
 
-    // Parent/Guardian Information-----------
-    parentGuardian: {
-      name: "",
-      address: "",
-      occupation: "",
-      contact: "",
-    },
-  });
+  //   // Parent/Guardian Information-----------
+  //   parentGuardian: {
+  //     name: "",
+  //     address: "",
+  //     occupation: "",
+  //     contact: "",
+  //   },
+  // });
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+  const [personalDetail, setPersonalDetail] = useState(
+    retrivePersonalDetails()
+  );
+
+  const handleChange = (e) => {
+    setPersonalDetail({ ...personalDetail, [e.target.name]: e.target.value });
   };
 
-  const handleParentChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      parentGuardian: {
-        ...prevData.parentGuardian,
-        [name]: value,
-      },
-    }));
+  // Handle Saved Alert
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    savePersonalDetails(personalDetail);
   };
 
-  const handleSave = () => {
-    onSave({ formData }); // Pass the data back to the parent
+  const [showAlert, setShowAlert] = useState(false);
+  const handleSave = (e) => {
+    e.preventDefault();
+
+    try {
+      savePersonalDetails(personalDetail); // Save the data
+      setShowAlert(true); // Show success alert
+      setTimeout(() => setShowAlert(false), 3000); // Hide after 3 seconds
+    } catch (error) {
+      console.error("Save failed:", error);
+    }
   };
 
   return (
-    <div
-      className={`bg-white p-6 rounded transform transition-transform duration-500 ${
-        isVisible ? "scale-100 opacity-100" : "scale-90 opacity-0"
-      }`}
-    >
-      <div className="flex flex-col md:flex-row justify-between items-center">
-        <h2 className="text-2xl font-semibold mb-4">Personal Details</h2>
-        {/* Profile Picture Upload */}
-        <div className="mb-4">
-          <ProfilePictureUpload preview={preview} setPreview={setPreview} />
-        </div>
-      </div>
-
-      {/* Name Fields */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-2">
-        <div>
-          <label className="block font-medium mb-2">Surname</label>
-          <input
-            type="text"
-            name="surname"
-            value={formData.surname}
-            onChange={handleInputChange}
-            className="w-full border rounded p-2"
-          />
-        </div>
-        <div>
-          <label className="block font-medium mb-2">First Name</label>
-          <input
-            type="text"
-            name="firstName"
-            value={formData.firstName}
-            onChange={handleInputChange}
-            className="w-full border rounded p-2"
-          />
-        </div>
-        <div>
-          <label className="block font-medium mb-2">Other Name</label>
-          <input
-            type="text"
-            name="otherName"
-            value={formData.otherName}
-            onChange={handleInputChange}
-            className="w-full border rounded p-2"
-          />
-        </div>
-        <div>
-          <label className="block font-medium mb-2">Gender</label>
-          <select
-            name="relationship"
-            value={formData.parentGuardian.relationship}
-            onChange={handleParentChange}
-            className="w-full border rounded p-2"
-          >
-            <option value=""></option>
-            <option value="Male">Male</option>
-            <option value="Female">Female</option>
-          </select>
-        </div>
-        <div>
-          <label className="block font-medium mb-2">Date of Birth</label>
-          <input
-            type="date"
-            name="dateOfBirth"
-            value={formData.dateOfBirth}
-            onChange={handleInputChange}
-            className="w-full border rounded p-2"
-          />
-        </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div>
-            <label className="block font-medium mb-2">Age</label>
-            <input
-              type="number"
-              name="age"
-              value={formData.age}
-              onChange={handleInputChange}
-              className="w-full border rounded p-2"
-            />
-          </div>
+    <div>
+      {showAlert && <OverlayAlert message="Data saved!" />}
+      <div
+        className={`bg-white p-6 rounded transform transition-transform duration-500 ${
+          isVisible ? "scale-100 opacity-100" : "scale-90 opacity-0"
+        }`}
+      >
+        <div className="flex flex-col md:flex-row justify-between items-center">
+          <h2 className="text-2xl font-semibold mb-4">Personal Details</h2>
+          {/* Profile Picture Upload */}
           <div className="mb-4">
-            <label className="block font-medium mb-2">Marital Status</label>
-            <select
-              name="maritalStatus"
-              value={formData.maritalStatus}
-              onChange={handleInputChange}
-              className="w-full border rounded p-2"
-            >
-              <option value="Single">Single</option>
-              <option value="Married">Married</option>
-            </select>
+            <ProfilePictureUpload preview={preview} setPreview={setPreview} />
           </div>
         </div>
-      </div>
 
-      {/* Date of Birth and Age ------*/}
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        {/* Languages Spoken*/}
-        <div>
-          <label className="block font-medium mb-2">Languages Spoken</label>
-          <input
-            type="text"
-            name="languagesSpoken"
-            value={formData.languagesSpoken}
-            onChange={handleInputChange}
-            className="w-full border rounded p-2"
-          />
-        </div>
-        <div>
-          <label className="block font-medium mb-2">Place of Birth</label>
-          <input
-            type="text"
-            name="placeOfBirth"
-            value={formData.placeOfBirth}
-            onChange={handleInputChange}
-            className="w-full border rounded p-2"
-          />
-        </div>
-        <div>
-          <label className="block font-medium mb-2">Religion</label>
-          <input
-            type="text"
-            name="religion"
-            value={formData.religion}
-            onChange={handleInputChange}
-            className="w-full border rounded p-2"
-          />
-        </div>
-        <div>
-          <label className="block font-medium mb-2">Hometown</label>
-          <input
-            type="text"
-            name="hometown"
-            value={formData.hometown}
-            onChange={handleInputChange}
-            className="w-full border rounded p-2"
-          />
-        </div>
-        <div>
-          <label className="block font-medium mb-2">District</label>
-          <input
-            type="text"
-            name="district"
-            value={formData.district}
-            onChange={handleInputChange}
-            className="w-full border rounded p-2"
-          />
-        </div>
-        <div>
-          <label className="block font-medium mb-2">Region</label>
-          <input
-            type="text"
-            name="region"
-            value={formData.region}
-            onChange={handleInputChange}
-            className="w-full border rounded p-2"
-          />
-        </div>
-      </div>
+        <form onSubmit={handleSubmit}>
+          {/* Name Fields */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-2">
+            <div>
+              <label className="block font-medium mb-2">Surname</label>
+              <input
+                type="text"
+                name="surname"
+                value={personalDetail.surname}
+                onChange={handleChange}
+                className="w-full border rounded p-2"
+              />
+            </div>
+            <div>
+              <label className="block font-medium mb-2">First Name</label>
+              <input
+                type="text"
+                name="firstName"
+                value={personalDetail.firstName}
+                onChange={handleChange}
+                className="w-full border rounded p-2"
+              />
+            </div>
+            <div>
+              <label className="block font-medium mb-2">Other Name</label>
+              <input
+                type="text"
+                name="otherName"
+                value={personalDetail.otherName}
+                onChange={handleChange}
+                className="w-full border rounded p-2"
+              />
+            </div>
+            <div>
+              <label className="block font-medium mb-2">Gender</label>
+              <select
+                name="gender"
+                value={personalDetail.gender}
+                onChange={handleChange}
+                className="w-full border rounded p-2"
+              >
+                <option value=""></option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+            </div>
+            <div>
+              <label className="block font-medium mb-2">Date of Birth</label>
+              <input
+                type="date"
+                name="dateOfBirth"
+                value={personalDetail.dateOfBirth}
+                onChange={handleChange}
+                className="w-full border rounded p-2"
+              />
+            </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              <div>
+                <label className="block font-medium mb-2">Age</label>
+                <input
+                  type="number"
+                  name="age"
+                  value={personalDetail.age}
+                  onChange={handleChange}
+                  className="w-full border rounded p-2"
+                />
+              </div>
+              <div className="mb-4">
+                <label className="block font-medium mb-2">Marital Status</label>
+                <select
+                  name="maritalStatus"
+                  value={personalDetail.maritalStatus}
+                  onChange={handleChange}
+                  className="w-full border rounded p-2"
+                >
+                  <option value="Single">Single</option>
+                  <option value="Married">Married</option>
+                </select>
+              </div>
+            </div>
+          </div>
 
-      {/* Parent/Guardian Details */}
-      <div className="mt-10">
-        <h2 className="text-xl font-semibold mb-3">
-          Parent/Guardian Information
-        </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-          <div>
-            <label className="block font-medium mb-2">Name</label>
-            <input
-              type="text"
-              name="name"
-              value={formData.parentGuardian.name}
-              onChange={handleParentChange}
-              className="w-full border rounded p-2"
-            />
+          {/* Date of Birth and Age ------*/}
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            {/* Languages Spoken*/}
+            <div>
+              <label className="block font-medium mb-2">Languages Spoken</label>
+              <input
+                type="text"
+                name="languagesSpoken"
+                value={personalDetail.languagesSpoken}
+                onChange={handleChange}
+                className="w-full border rounded p-2"
+              />
+            </div>
+            <div>
+              <label className="block font-medium mb-2">Place of Birth</label>
+              <input
+                type="text"
+                name="placeOfBirth"
+                value={personalDetail.placeOfBirth}
+                onChange={handleChange}
+                className="w-full border rounded p-2"
+              />
+            </div>
+            <div>
+              <label className="block font-medium mb-2">Religion</label>
+              <input
+                type="text"
+                name="religion"
+                value={personalDetail.religion}
+                onChange={handleChange}
+                className="w-full border rounded p-2"
+              />
+            </div>
+            <div>
+              <label className="block font-medium mb-2">Hometown</label>
+              <input
+                type="text"
+                name="hometown"
+                value={personalDetail.hometown}
+                onChange={handleChange}
+                className="w-full border rounded p-2"
+              />
+            </div>
+            <div>
+              <label className="block font-medium mb-2">District</label>
+              <input
+                type="text"
+                name="district"
+                value={personalDetail.district}
+                onChange={handleChange}
+                className="w-full border rounded p-2"
+              />
+            </div>
+            <div>
+              <label className="block font-medium mb-2">Region</label>
+              <input
+                type="text"
+                name="region"
+                value={personalDetail.region}
+                onChange={handleChange}
+                className="w-full border rounded p-2"
+              />
+            </div>
           </div>
-          <div>
-            <label className="block font-medium mb-2">Occupation</label>
-            <input
-              type="text"
-              name="occupation"
-              value={formData.parentGuardian.occupation}
-              onChange={handleParentChange}
-              className="w-full border rounded p-2"
-            />
+
+          {/* Parent/Guardian Details */}
+          <div className="mt-10">
+            <h2 className="text-xl font-semibold mb-3">
+              Parent/Guardian Information
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              <div>
+                <label className="block font-medium mb-2">Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  value={personalDetail.name}
+                  onChange={handleChange}
+                  className="w-full border rounded p-2"
+                />
+              </div>
+              <div>
+                <label className="block font-medium mb-2">Occupation</label>
+                <input
+                  type="text"
+                  name="occupation"
+                  value={personalDetail.occupation}
+                  onChange={handleChange}
+                  className="w-full border rounded p-2"
+                />
+              </div>
+              <div>
+                <label className="block font-medium mb-2">Contact</label>
+                <input
+                  type="text"
+                  name="contact"
+                  value={personalDetail.contact}
+                  onChange={handleChange}
+                  className="w-full border rounded p-2"
+                />
+              </div>
+              <div>
+                <label className="block font-medium mb-2">Address</label>
+                <input
+                  type="text"
+                  name="address"
+                  value={personalDetail.address}
+                  onChange={handleChange}
+                  className="w-full border rounded p-2"
+                />
+              </div>
+            </div>
           </div>
-          <div>
-            <label className="block font-medium mb-2">Contact</label>
-            <input
-              type="text"
-              name="contact"
-              value={formData.parentGuardian.contact}
-              onChange={handleParentChange}
-              className="w-full border rounded p-2"
-            />
+          <div className="mt-5">
+            <SaveButton onClick={handleSave} />
           </div>
-          <div>
-            <label className="block font-medium mb-2">Address</label>
-            <input
-              type="text"
-              name="address"
-              value={formData.parentGuardian.address}
-              onChange={handleParentChange}
-              className="w-full border rounded p-2"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="mt-5">
-        <SaveButton onClick={() => handleSave("personalDetails", formData)} />
+        </form>
       </div>
     </div>
   );
