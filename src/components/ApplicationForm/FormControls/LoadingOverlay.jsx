92cd16d014aa9b3e437131c48bfce999e4d11e05
Tyler Gobka Bright <tgbright88@gmail.com>
2024-12-19 14:28:01 +0000

    developing...

--- src/components/ApplicationForm/FormControls/LoadingOverlay.jsx
+++ src/components/ApplicationForm/FormControls/LoadingOverlay.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import {
-  AiOutlineLoading3Quarters,
-  // AiOutlineCheckCircle,
-} from "react-icons/ai";
+import {AiOutlineLoading3Quarters} from "react-icons/ai";
 
 const LoadingOverlay = ({ message }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
       <div className="bg-white rounded-lg p-6 shadow-lg flex flex-col items-center">
-        <AiOutlineLoading3Quarters className="animate-spin text-primary text-4xl mb-4" />
-
-        {/* NOTE! This's a "check mark" to show for successful "messages"// Uncomment it and render the condition to show together with "Successful!" Message */}
-        {/* <AiOutlineCheckCircle className="text-4xl" color="green" /> */}
-        <p className="text-gray-800 text-lg font-semibold text-center">
+        <AiOutlineLoading3Quarters className="animate-spin text-primary text-4xl" />
+        <p className="text-gray-800 text-sm font-semibold text-center mt-2">
           {message}
         </p>
       </div>
